d20c046b51bbbc2eac63e1e248d0fbc50994f32d
bezasrat <bezahun33@gmail.com>
2022-09-22 19:08:23 -0700

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} arguments.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
+
 if __name__ == "__main__":
     import sys
-
-
-if len(sys.argv) == 1:    
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-if len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-if len(sys.argv) > 2:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-if len(sys.argv):
-    for i in range(1, len(sys.argv)):
-        print("{:d}:".format(i), sys.argv[i])
+    print_arg(sys.argv)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+def infAdd(argv):
+    le = len(argv) - 1
+    sum = 0
+    if le > 0:
+        for i in range(1, le + 1):
+            sum += int(argv[i])
+    print(f"{sum}")
+
+
 if __name__ == "__main__":
     import sys
-        
-answer = 0
-
-for i in range(1, len(sys.argv)):
-    answer += int(sys.argv[i])
-print("{:d}".format(answer))
+    infAdd(sys.argv)
