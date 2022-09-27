00a709202767668380d2cc1c8a65ac71cc0d16b7
bezasrat <bezahun33@gmail.com>
2022-09-26 18:40:38 -0700

    Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    str_len = len(sentence)
+    if len(sentence) <= 0:
+        first_char = "None"
+    else:
+        first_char = sentence[0]
+    return (str_len, first_char)
