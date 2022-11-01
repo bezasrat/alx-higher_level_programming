7db33cfce02e7b422cf9aaf7cff2b58ba6e38950
bezasrat <bezahun33@gmail.com>
2022-10-31 21:30:31 -0700

    Python - Inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+File: 5-base_geometry.py
+Desc: This module defines an empty class.
+Author: Bezahun
+Date Created: Nov 1 2022
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class definition.
+    """
